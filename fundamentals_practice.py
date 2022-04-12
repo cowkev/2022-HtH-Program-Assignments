8d78784ea715ab286df006e12cf35450203fb904
cowkev <92410665+cowkev@users.noreply.github.com>
2022-04-12 16:48:27 -0700

    fundamentals practice

---
+++ fundamentals_practice.py
@@ -0,0 +1,17 @@
+print ("Hello World") #"Hello World"
+name = "Kevin"
+age = 18
+years_at_hth = .5
+plays_basketball = False
+jersey_number = "09"
+car_name = "Toyota", "Acura", "Ford", "Tesla"
+x = 50
+my_first_name = "John"
+print (name)
+print (age)
+print (years_at_hth)
+print (plays_basketball)
+print (jersey_number)
+print (car_name)
+print (x)
+print (my_first_name)
