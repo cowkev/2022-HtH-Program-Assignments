d872ebd083c64eb038aa4cd43047571ac336ce6e
cowkev <92410665+cowkev@users.noreply.github.com>
2022-04-27 16:22:42 -0700

    update dictionary practice

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -6,3 +6,42 @@ one = "Irvine"
 
 perentage_irvine = Acceptance["Irvine"]
 print(perentage_irvine)
+
+Sneakers= {"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+Jordan_Stock = Sneakers["Jordan 13"]
+print(Jordan_Stock)
+
+Sneakers["SB Dunk"] -= 2
+print("\nCustomer came to purchase 2 pairs of SB Dunks")
+print(Sneakers["SB Dunk"])
+
+Sneakers["Yeezy"] -=1
+print("\nCustomer returned a pair of Yeezys")
+print(Sneakers["Yeezy"])
+
+Sneakers["Air Max"] +=7
+Sneakers["Foamposite"] +=7
+Sneakers["Jordan 13"] +=7
+Sneakers["SB Dunk"] +=7
+Sneakers["Yeezy"] +=7
+print("\nNew shipment. All stock increased by 7")
+print(Sneakers)
+
+Sneakers["Air Max"] -=3
+Sneakers["Foamposite"] -=3
+Sneakers["Jordan 13"] -=3
+Sneakers["SB Dunk"] -=3
+Sneakers["Yeezy"] -=3
+print("\nStore Sale, all stock decreased by 3")
+print(Sneakers)
+
+Sneakers.update({"NMD": 6})
+Sneakers.update({"Vans": 12})
+Sneakers.update({"Uggs": 4})
+print("\nAdding shoes with update( ) method")
+print(Sneakers)
+
+del Sneakers["Uggs"]
+del Sneakers["NMD"]
+print("\nDeleted NMD and Uggs")
+print(Sneakers)
