401759fa83dd0b16c92c07c21c5f5548cc3f3cb7
cowkev <92410665+cowkev@users.noreply.github.com>
2022-04-28 11:08:13 -0700

    finished rock paper scissors

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,8 +1,8 @@
 Acceptance = {"Irvine" : "29.9 percent", "Los Angeles" : "14.3 percent", "San Diego" : "36.6 percent" }
 one = "Irvine"
-#print ("UCI acceeptance is: " + str(Acceptance.get ("Irvine")))
-#print ("UCLA acceptance is: " + str(Acceptance.get ("Los Angeles")))
-#print ("UCSD acceptance is: " + str(Acceptance.get ("San Diego")))
+print ("UCI acceeptance is: " + str(Acceptance.get ("Irvine")))
+print ("UCLA acceptance is: " + str(Acceptance.get ("Los Angeles")))
+print ("UCSD acceptance is: " + str(Acceptance.get ("San Diego")))
 
 perentage_irvine = Acceptance["Irvine"]
 print(perentage_irvine)
